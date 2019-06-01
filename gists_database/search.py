620c73246562de713d16777278d055861f6b445a
YuchuanWeng <cts734@gmail.com>
2019-06-01 13:34:10 +0000

    modify importer.py and search.py for tests passing

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,22 @@
 from .models import Gist
+import datetime
 
 def search_gists(db_connection, **kwargs):
-    pass
+    
+    query = 'SELECT * FROM gists'
+    if not kwargs:
+        cursor = db_connection.execute(query)
+        return cursor.fetchall()
+    else:
+        if "created_at" in kwargs and "github_id" in kwargs:
+            cursor = db_connection.execute("SELECT * FROM gists WHERE github_id = :github_id AND datetime(created_at) == datetime(:created_at)", kwargs)
+        if "created_at" in kwargs:
+            cursor = db_connection.execute("SELECT * FROM gists WHERE datetime(created_at) == datetime(:created_at)", kwargs)
+        if "github_id" in kwargs:
+            cursor = db_connection.execute("SELECT * FROM gists WHERE github_id = :github_id", kwargs)
+
+        results = []
+        for gist in cursor:
+            results.append(Gist(gist))
+        return results
+
